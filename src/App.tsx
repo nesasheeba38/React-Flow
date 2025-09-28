f29ede4a65a43bd6de89fa8b865604ce3cc7cf67
nesasheeba38 <nesasheebaj32@gmail.com>
2025-09-28 12:41:34 +0530

    inital file

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactFlow, { Background, Controls, MiniMap} from "reactflow";
+import "reactflow/dist/style.css";
+import{ Nodes }from './WorkFlow/Node';
+import{ Edges }from'./WorkFlow/Edges';
+
+function CustomNode({data}:any){
+  return(
+    <div
+    style={{
+      fontSize:15,
+      padding:10,
+      margin:20,
+      width:268,
+      border:"2px solid #f0e9e9ff",
+      backgroundColor:"#ffffffff",
+      borderRadius:10,
+    }}>
+   <p>{data.label}</p>
+   <button
+   style={{
+    padding:"8px 10px",
+    backgroundColor:"#f0eeeeff",
+    borderRadius:6,
+    width:260,
+    border:"2px solid white",
+    fontStyle:"serif"
+   }}>Beginner Project Ideas</button>   
+    </div>
+  )
+}
+
+export default function App() {
+  const nodeType={custom:CustomNode};
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <ReactFlow nodes={Nodes} edges={Edges} nodeTypes={nodeType}fitView>
+       <Background/>
+        <MiniMap/>
+        <Controls/> 
+      </ReactFlow>
+    </div>
+  );
+}
